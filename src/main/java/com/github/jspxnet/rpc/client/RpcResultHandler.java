c433126ce36c60131541025e7d0c7087b9e768b2
chenyuan <39793751@qq.com>
2020-06-15 19:23:46 +0800

    二维码例子

---
+++ src/main/java/com/github/jspxnet/rpc/client/RpcResultHandler.java
@@ -0,0 +1,12 @@
+package com.github.jspxnet.rpc.client;
+
+import io.netty.channel.ChannelHandlerContext;
+
+public class RpcResultHandler {
+
+    public static void invokeService(ChannelHandlerContext ct, IocResponse str)
+    {
+
+    }
+
+}
