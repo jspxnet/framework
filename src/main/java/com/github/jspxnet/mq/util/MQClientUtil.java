669cd1a64aa8ef72951f84da04dd03fed6c396b9
chenyuan <39793751@qq.com>
2020-12-23 12:35:01 +0800

    开始支持消息服务器

--- src/main/java/com/github/jspxnet/mq/util/MQClientUtil.java
+++ src/main/java/com/github/jspxnet/mq/util/MQClientUtil.java
@@ -2,19 +2,25 @@ package com.github.jspxnet.mq.util;
 
 import org.apache.rocketmq.client.ClientConfig;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.impl.MQClientAPIImpl;
 import org.apache.rocketmq.client.impl.MQClientManager;
 import org.apache.rocketmq.client.impl.factory.MQClientInstance;
 
 /**
  * Created by jspx.net
  *
- * @author: chenYuan
- * @date: 2020/12/22 0:54
- * @description: jspbox
+ * author: chenYuan
+ * date: 2020/12/22 0:54
+ * description: jspbox
  **/
 public class MQClientUtil {
 
+    /**
+     *  用完记得关闭
+     *  getMQClientInstance(producer).getMQClientAPIImpl();
+     * @param producer 可以是消息生产这
+     * @return 客户端接口, 主要是得到 MQClientAPIImpl
+     * @throws MQClientException 异常
+     */
     public static MQClientInstance getMQClientInstance(ClientConfig producer) throws MQClientException {
         MQClientManager mqClientManager = MQClientManager.getInstance();
         MQClientInstance mqClientInstance = mqClientManager.getOrCreateMQClientInstance(producer);
@@ -22,7 +28,5 @@ public class MQClientUtil {
         return mqClientInstance;
     }
 
-    public static MQClientAPIImpl getMQClientAPI(ClientConfig producer) throws MQClientException {
-        return getMQClientInstance(producer).getMQClientAPIImpl();
-    }
+
 }
