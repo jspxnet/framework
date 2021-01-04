9e3ad24994b434c63c8c71299f0be3f71c36e4f6
chenyuan <39793751@qq.com>
2021-01-04 23:48:33 +0800

    分布式通讯调整

--- src/main/java/com/github/jspxnet/network/rpc/env/MasterSocketAddress.java
+++ src/main/java/com/github/jspxnet/network/rpc/env/MasterSocketAddress.java
@@ -1,21 +1,13 @@
 package com.github.jspxnet.network.rpc.env;
 
-import com.github.jspxnet.boot.EnvFactory;
-import com.github.jspxnet.cache.redis.RedissonClientConfig;
 import com.github.jspxnet.enums.YesNoEnumType;
 import com.github.jspxnet.network.rpc.model.route.RouteChannelManage;
 import com.github.jspxnet.network.rpc.model.route.RouteSession;
-import com.github.jspxnet.sioc.BeanFactory;
 import com.github.jspxnet.utils.IpUtil;
 import com.github.jspxnet.utils.ObjectUtil;
-import com.github.jspxnet.utils.StringUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RBucket;
-import org.redisson.api.RedissonClient;
-
 import java.net.SocketAddress;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jspx.net
@@ -26,9 +18,7 @@ import java.util.concurrent.TimeUnit;
  **/
 @Slf4j
 public class MasterSocketAddress {
-    //保存一分到redis
-    private static final String ADDRESS_LIST_KEY = "rpc:master:group:list";
-    private static final Map<String,List<SocketAddress>> groupListMap = new HashMap<>();
+    private static final Map<String,List<SocketAddress>> GROUP_LIST_MAP = new HashMap<>();
     private static int current = 0;
     final private static MasterSocketAddress INSTANCE = new  MasterSocketAddress();
     public static MasterSocketAddress getInstance()
@@ -36,8 +26,6 @@ public class MasterSocketAddress {
         return INSTANCE;
     }
 
-    //@Ref(bind = RedissonClientConfig.class)
-    static RedissonClient redissonClient;
 
     private MasterSocketAddress()
     {
@@ -50,23 +38,21 @@ public class MasterSocketAddress {
         {
             List<SocketAddress> defaultSocketAddressList = Collections.synchronizedList(new ArrayList<>());
             defaultSocketAddressList.addAll(RpcConfig.getInstance().getMasterGroupList(name));
-            groupListMap.put(name,defaultSocketAddressList);
+            GROUP_LIST_MAP.put(name,defaultSocketAddressList);
         }
-        BeanFactory beanFactory = EnvFactory.getBeanFactory();
-        redissonClient = (RedissonClient)beanFactory.getBean(RedissonClientConfig.class);
     }
 
     synchronized public SocketAddress getSocketAddress(String serviceName)
     {
-        if (ObjectUtil.isEmpty(groupListMap))
+        if (ObjectUtil.isEmpty(GROUP_LIST_MAP))
         {
             log.error("RPC服务调用,没有配置服务器地址列表");
             return null;
         }
-        List<SocketAddress> defaultSocketAddressList = groupListMap.get(serviceName);
+        List<SocketAddress> defaultSocketAddressList = GROUP_LIST_MAP.get(serviceName);
         if (ObjectUtil.isEmpty(defaultSocketAddressList))
         {
-            defaultSocketAddressList = groupListMap.get("default");
+            defaultSocketAddressList = GROUP_LIST_MAP.get("default");
         }
         if (ObjectUtil.isEmpty(defaultSocketAddressList))
         {
@@ -77,19 +63,6 @@ public class MasterSocketAddress {
         if (current>=defaultSocketAddressList.size())
         {
             current = 0;
-            if (redissonClient!=null)
-            {
-                RBucket<String> bucket = redissonClient.getBucket(ADDRESS_LIST_KEY);
-                if (bucket!=null)
-                {
-                    String ips =  bucket.get();
-                    if (!StringUtil.isEmpty(ips))
-                    {
-                        defaultSocketAddressList.clear();
-                        defaultSocketAddressList.addAll(IpUtil.getSocketAddressList(ips));
-                    }
-                }
-            }
         }
         try {
             return defaultSocketAddressList.get(current);
@@ -99,7 +72,7 @@ public class MasterSocketAddress {
     }
 
     public List<SocketAddress> getDefaultSocketAddressList(String serviceName) {
-        List<SocketAddress> defaultSocketAddressList = groupListMap.get(serviceName);
+        List<SocketAddress> defaultSocketAddressList = GROUP_LIST_MAP.get(serviceName);
         List<SocketAddress> result = new ArrayList<>();
         for (SocketAddress socketAddress:defaultSocketAddressList)
         {
@@ -110,7 +83,7 @@ public class MasterSocketAddress {
     }
 
     public List<String> getDefaultSocketAddressGroupNames() {
-        return new ArrayList<>(groupListMap.keySet());
+        return new ArrayList<>(GROUP_LIST_MAP.keySet());
     }
     /**
      * 将路由表放入请求缓存中
@@ -122,41 +95,31 @@ public class MasterSocketAddress {
         {
             return;
         }
-
+        List<SocketAddress> saveList = new ArrayList<>();
         for (String groupName:serviceNames)
         {
-            StringBuilder ips = new StringBuilder();
-            List<RouteSession>  list = routeChannelManage.getRouteSessionList();
-            if (redissonClient==null)
-            {
-                List<SocketAddress> defaultSocketAddressList = groupListMap.get(groupName);
-                defaultSocketAddressList.clear();
-            }
+            List<RouteSession> list = routeChannelManage.getRouteSessionList();
             for (RouteSession session:list)
             {
-                if (YesNoEnumType.YES.getValue()==session.getOnline())
+                if (YesNoEnumType.NO.getValue()==session.getOnline())
                 {
-                    if (redissonClient==null)
-                    {
-                        List<SocketAddress> defaultSocketAddressList = groupListMap.get(groupName);
-                        if (defaultSocketAddressList==null)
-                        {
-                            defaultSocketAddressList = new ArrayList<>();
-                        }
-                        defaultSocketAddressList.add(session.getSocketAddress());
-                    }
-                    ips.append(IpUtil.getIp(session.getSocketAddress())).append(StringUtil.SEMICOLON);
+                    continue;
                 }
+                saveList.add(session.getSocketAddress());
             }
-            if (ips.toString().endsWith(StringUtil.SEMICOLON))
+            if (!ObjectUtil.isEmpty(list))
             {
-                ips.setLength(ips.length()-1);
-            }
-            if (redissonClient!=null&&!redissonClient.getBucket(ADDRESS_LIST_KEY).isExists())
-            {
-                redissonClient.getBucket(ADDRESS_LIST_KEY).set(ips.toString(),RpcConfig.getInstance().getTimeout()*3, TimeUnit.SECONDS);
+                List<SocketAddress> defaultSocketAddressList = GROUP_LIST_MAP.get(groupName);
+                if (defaultSocketAddressList==null)
+                {
+                    defaultSocketAddressList = new ArrayList<>();
+                }
+                defaultSocketAddressList.clear();
+                defaultSocketAddressList.addAll(saveList);
             }
         }
+
+
 
     }
 
