a40a6b11acdc70ae754b054832c403ab9c28b006
chenyuan <39793751@qq.com>
2021-01-26 19:08:50 +0800

    线程控制调整

--- src/main/java/com/github/jspxnet/mq/util/MQClientUtil.java
+++ src/main/java/com/github/jspxnet/mq/util/MQClientUtil.java
@@ -15,13 +15,14 @@ import org.apache.rocketmq.client.impl.factory.MQClientInstance;
 public class MQClientUtil {
 
     /**
+     * 得到mq的高级功能,一般用得不多
      *  用完记得关闭
      *  getMQClientInstance(producer).getMQClientAPIImpl();
      * @param producer 可以是消息生产这
      * @return 客户端接口, 主要是得到 MQClientAPIImpl
      * @throws MQClientException 异常
      */
-    public static MQClientInstance getMQClientInstance(ClientConfig producer) throws MQClientException {
+    public static MQClientInstance getMqClientInstance(ClientConfig producer) throws MQClientException {
         MQClientManager mqClientManager = MQClientManager.getInstance();
         MQClientInstance mqClientInstance = mqClientManager.getOrCreateMQClientInstance(producer);
         mqClientInstance.start();
