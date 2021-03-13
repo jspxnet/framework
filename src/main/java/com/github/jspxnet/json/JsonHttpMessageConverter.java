035866a38cf15e2ca2341d87152365ee7a02152a
chenyuan <39793751@qq.com>
2021-03-14 02:09:10 +0800

    简化pom

---
+++ src/main/java/com/github/jspxnet/json/JsonHttpMessageConverter.java
@@ -0,0 +1,29 @@
+package com.github.jspxnet.json;
+
+
+import org.springframework.http.converter.json.AbstractJsonHttpMessageConverter;
+import org.springframework.lang.Nullable;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+
+/**
+ * Created by chenYuan
+ *
+ * @author: chenYuan
+ * @date: 2021/3/10 22:17
+ * @description: springbase
+ **/
+public class JsonHttpMessageConverter extends AbstractJsonHttpMessageConverter {
+
+    @Override
+    protected Object readInternal(Type resolvedType, Reader reader) throws Exception {
+        return  GsonUtil.createGson().fromJson(reader,resolvedType);
+    }
+
+    @Override
+    protected void writeInternal(Object object, @Nullable Type type, Writer writer) throws Exception {
+        JSONObject json = new JSONObject(object);
+        writer.write(json.toString(4));
+    }
+}
