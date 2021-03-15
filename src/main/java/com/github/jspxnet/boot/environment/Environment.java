d936c3d0e47baa88090e8949c115a38099500def
chenyuan <39793751@qq.com>
2021-03-15 19:08:21 +0800

    简化pom

--- src/main/java/com/github/jspxnet/boot/environment/Environment.java
+++ src/main/java/com/github/jspxnet/boot/environment/Environment.java
@@ -150,7 +150,7 @@ public abstract class Environment {
     final static public String myDomainName = "<a href=\"http://www.jspx.net\">www.jspx.net</a>";
 
     //版本号
-    final static public String version = "6.36";
+    final static public String version = "6.37";
 
     //发布许可方式
     final static public String licenses = "AGPLv3";
